40850b46d7c1c02db48cb68c787cf41f6569e0b9
saranfly <yangbenjob@sina.com>
2016-04-28 00:06:18 +0800

    modify main function

--- ApapterMode/main.cpp
+++ ApapterMode/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include "textshape.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //类适配器方式
+    TextShape test;
+    Manipulator *ManipulatorObject = test.CreatManipulator();
+
+/*********************************************************************************************/
+/********************************    华丽的分割线    *******************************************/
+/*********************************************************************************************/
+
+    //对象适配器方式
+    TextView *textviewObject = new TextView();
+    TextShape test(textviewObject);
+    Manipulator *ManipulatorObject = test.CreatManipulator();
     return 0;
 }
 
